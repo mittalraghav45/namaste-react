1fa03b7d6ccfeda1a89d2e587d1cae308b23bd4d
Raghav Mittal <mittalraghav45@gmail.com>
2025-08-14 22:00:28 +0100

    code

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import "./index.css";
+//React Element is an object
+
+const jsxHeading=  <h1 className="heading">this is jsx heding</h1>;
+
+root.render(jsxHeading);
